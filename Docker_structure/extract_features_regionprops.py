6f6fe055ebcdcf80d3d77347af8d4d8b38777d17
Helena-todd <helena_t@hotmail.fr>
2023-07-25 15:41:16 +0200

    parallelised version of the extract features function

--- Docker_structure/extract_features_regionprops.py
+++ Docker_structure/extract_features_regionprops.py
@@ -4,42 +4,27 @@ import pandas as pd
 from skimage import io
 import sys
 import glob
+import dask
+from helper import compute_centroids
 
 # scipy package to compute distance between matrices of cell centroid coordinates
 from scipy.spatial.distance import cdist
 # skimage package to identify objects, export region properties
 from skimage.measure import label, regionprops, regionprops_table
 
-# List files in current folder
-files_list = sorted(glob.glob("*.tif"))
 
-# Get variables from system env
-output_dir = sys.argv[1]
-
-for tp in range(0,len(files_list)):
+@dask.delayed
+def _compute(files_list, tp, output_dir):
     #print('Processing file ', files_list[tp])
     img1 = io.imread(files_list[tp])
     
     # See how many cells were identified in the images (ranged in the "unique" vectors)
     unique1, counts1 = np.unique(img1, return_counts=True)
-    # In registered images, it can happen that one cell gets skipped in the unique1 array, which leads to number of cells in unique1 and counts1 NOT MATCHING the unique1 indices. I thus create a unique11 array with continuous cell indices, to be used to track daughters that are still unmatched.
-    #unique11 = np.array(range(0, len(unique1)))
-    #unique0, counts0 = np.unique(img0, return_counts=True)
-    #unique01 = np.array(range(0, len(unique0)))
     
     ###################################################
     # compute the mask centroids of all cells in img1 #
     ###################################################
-    my_array1 = np.empty((0,2), int)
-    for mask_tmp in unique1[1:]: # [:1] because I'm not interested in pixels with a value of 0: the background
-        # Keep only current mask, replace all other values by 0
-        img_tmp = np.where(img1 != mask_tmp, 0, img1)
-    
-        # compute cell centroid
-        M = cv2.moments(img_tmp)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        my_array1=np.append(my_array1,[[cx,cy]], axis=0)
+    my_array1 = compute_centroids(unique1, img1)
     
     ###################################################################################
     # Compute distances between cells (in a similar way to R's pdist::pdist function) #
@@ -71,4 +56,19 @@ for tp in range(0,len(files_list)):
     # Export feature cell information for the current image #
     #########################################################
     data_tmp.to_csv(output_dir + f"feature_table_time{tp}.csv")
+
+
+
+def _extract_features(files_list, output_dir):
+    delayed_results = []
+    for tp in range(0,len(files_list)):
+        delayed_results.append(_compute(files_list, tp, output_dir))
+    dask.compute(delayed_results, scheduler='threads')
+
+if __name__=="__main__":
+    files_list = sorted(glob.glob("*.tif"))
+    output_dir = sys.argv[1]
+    _extract_features(files_list, output_dir)
+
+
 
